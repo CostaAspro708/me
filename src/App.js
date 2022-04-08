db90a7548faddfaf20e319c98a6209182a15c1d8
CostaAspro708 <n10464174@qut.edu.au>
2022-04-09 01:22:04 +1000

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div class="logo">
+            Costa Aspromourgos
+          </div>
+        <nav>
+          <ul>
+            <li><a>1</a></li>
+            <li><a>2</a></li>
+            <li><a>3</a></li>
+            <li><a>4</a></li>
+          </ul>
+        </nav>
       </header>
+      <body className="App-body">
+        body
+      </body>
     </div>
   );
 }
